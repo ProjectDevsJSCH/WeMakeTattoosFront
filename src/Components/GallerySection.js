019c9deea270eda41b124de2b207a40bd12a8d6b
Juan Castelblanco <jscastelblancoh@unal.edu.co>
2019-08-22 16:56:05 -0500

    Previsualización completa

--- src/Components/GallerySection.js
+++ src/Components/GallerySection.js
@@ -5,8 +5,6 @@ import ImageContainer from './ImageContainer';
 import MasonryLayout from "./MasonryLayout";
 import Pagination from "react-js-pagination";
 
-
-import t6 from "../img/t6.jpg";
 import './styles/GallerySection.css';
 import Avatar from "../img/avatar-icon.png"
 
@@ -17,9 +15,10 @@ class GallerySection extends React.Component {
         categories: [],
         actualCategory: 1,
         actualPage: 1,
-        numberOfImages: 0
+        numberOfImages: 0,
+        previewImage: ""
     }
-    
+
     firstPagePerCategory() {
         let page = 1;
         axios({
@@ -32,18 +31,18 @@ class GallerySection extends React.Component {
         }).catch(error => {
         });
     }
-    
-    fetchImagesPerPage(){
+
+    fetchImagesPerPage() {
         axios({
             url: `https://localhost:44376/api/TatoosByCategory/${this.state.actualPage}/${this.state.actualCategory}`,
             method: `get`
         }).then(result => {
-            this.setState({images: result.data.list});
+            this.setState({ images: result.data.list });
         }).catch({
-            
+
         });
     }
-    
+
     handlePageChange(pageNumber) {
         this.setState({ actualPage: pageNumber }, () => { this.fetchImagesPerPage() });
     }
@@ -68,6 +67,10 @@ class GallerySection extends React.Component {
         });
     }
 
+    changePreviewImage(urlImage){
+        this.setState({previewImage: urlImage})
+    }
+
     componentDidMount() {
         this.firstPagePerCategory();
         this.fetchCategories();
@@ -78,20 +81,20 @@ class GallerySection extends React.Component {
             <div className="Gallerybackground container-fluid">
                 <div className="row pl-5">
                     <div className="Categorybutton col-4">
-                        <select ref="SelectCategory" className="browser-default custom-select" onChange={() => { this.setState({actualCategory: this.refs.SelectCategory.value}, () => {this.firstPagePerCategory()} )}}>
+                        <select ref="SelectCategory" className="browser-default custom-select" onChange={() => { this.setState({ actualCategory: this.refs.SelectCategory.value }, () => { this.firstPagePerCategory() }) }}>
                             {this.state.categories.map(category => (
-                                <option value={category.IdCategory}>{category.CategoryName}</option>
+                                <option key={category.IdCategory} value={category.IdCategory}>{category.CategoryName}</option>
                             ))}
                         </select>
 
                     </div>
                 </div>
-                <div className="Gallerysection row">
-                    <div className="Galleryimgs col-md-8 d-flex flex-column">
+                <div className="Gallerysection row d-flex justify-content-center align-items-center">
+                    <div className="Galleryimgs col-lg-5 d-flex flex-column">
                         <div className="flex-fill">
                             <MasonryLayout columns={3} gap={25}>
                                 {this.state.images.map(url => (
-                                    <ImageContainer key={url.TattoosByArtistId} URLImage={url.TattooImgUrl} />
+                                    <ImageContainer key={url.TattoosByArtistId} URLImage={url.TattooImgUrl} imageSelected={this.changePreviewImage.bind(this)}/>
                                 ))}
                             </MasonryLayout>
                         </div>
@@ -108,16 +111,18 @@ class GallerySection extends React.Component {
                             />
                         </div>
                     </div>
-                    <div className="Imgpreview col-md-4">
-                        <div className="card" >
-                            <img className="card-img-top" src={t6} alt="Card image cap"></img>
-                            <div className="card-body">
-                                <div className='ArtistProfile'>
-                                    <div className='ArtistAvatar'>
-                                        <img src={Avatar} alt="avatar1" />
+                    <div id="PreviewCard" className="Imgpreview col-lg-5 d-flex justify-content-center align-items-center">
+                        <div className="row">
+                            <div className="card col-10" >
+                                <img className="card-img-top" src={this.state.previewImage} alt="Card"></img>
+                                <div className="card-body-artist card-body d-flex justify-content-center">
+                                    <div className='ArtistProfile'>
+                                        <div className='ArtistAvatar'>
+                                            <img src={Avatar} alt="avatar1" />
+                                        </div>
+                                        <div className='ArtistName'>
+                                            artista 1
                                     </div>
-                                    <div className='ArtistName'>
-                                        artista 1
                                     </div>
                                 </div>
                             </div>
--- src/Components/ImageContainer.js
+++ src/Components/ImageContainer.js
@@ -2,9 +2,15 @@ import React from 'react';
 import "./styles/ImageContainer.css";
 
 class ImageContainer extends React.Component {
+
+   imageClicked = (e, url) => {
+      // e.preventDefault();
+      this.props.imageSelected(url);
+   }
+
    render() {
       return (
-         <img className="img-thumbnail ImageLoaded" src={this.props.URLImage} alt="Renderización" />
+         <a ref="linkImg" href="#PreviewCard" onClick={(e) => this.imageClicked(e, this.props.URLImage)}><img className="img-thumbnail ImageLoaded" src={this.props.URLImage} alt="Renderización" /></a>
       );
    }
 }
